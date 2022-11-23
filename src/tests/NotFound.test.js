7e936d06e5d264c83be0d90c53f4a17c8e51e0df
Carlos Vieira <carlepsvieira@gmail.com>
2022-11-23 10:01:32 -0300

    test 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('testing component <NotFound.js />', () => {
+  test('page must have an h2 element with text "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    const h2Element = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    expect(h2Element).toBeInTheDocument();
+  });
+
+  test('page must have an img element with specific src', () => {
+    renderWithRouter(<NotFound />);
+    const imgElement = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+      src: 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    });
+    expect(imgElement).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
