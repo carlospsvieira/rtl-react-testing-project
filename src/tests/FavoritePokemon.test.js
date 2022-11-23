37d9354a9dd9af6f4a34d83dcebc381b9906883b
Carlos Vieira <carlepsvieira@gmail.com>
2022-11-23 09:48:16 -0300

    test 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemon from '../pages/FavoritePokemon';
+
+describe('testing component <FavoritePokemon.js />', () => {
+  test('page must have text "No favorite pokemon found" in case there is not a favorite pokemon, otherwise all favorite pokemons', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const notFavorite = screen.getByText(/no favorite pokémon found/i);
+    expect(notFavorite).toBeInTheDocument();
+  });
+});
